9e3176882ede8276e3bb85b12460e2b500d7c4b9
VladMatviyuk <vladm1225@yandex.ru>
2017-05-28 19:36:17 +0300

    loko start

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,7 @@
-// $(document).foundation();
+$(document).ready(function() {
+//$(document).foundation();
+
+
+  var heightNavbar = +($('.lok-navbar').height());
+  $('body').find('.lok-header').css('height', ('calc(100vh - ' + heightNavbar + 'px)' ));
+});
